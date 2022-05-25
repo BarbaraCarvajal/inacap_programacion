98e67885a0479b7d3fdfd1e82fa10ea231a123df
leoarayav <laraya1818@gmail.com>
2022-05-25 18:18:07 -0400

    fix: All changes were updated

---
+++ python_funciones_etc/ejercicios/1_.py
@@ -0,0 +1,11 @@
+"""Crear  una  función  que  permita  evaluar  
+2   valores  enteros  ingresados  por  teclado, 
+ inicio  y  final  de  una 
+secuencia, luego muestre todos los valores que hay entre 
+esos dos límites ingresados (ambos valores inclusive) 
+separados por guión. Utilice ciclos while y variable tipo switch, 
+validando que el valor final de la secuencia sea 
+mayor que el inicial. """
+
+
+
